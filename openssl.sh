9d9d2267560cb77bea486c87e312a2072035ec80
xw_y_am <xw_y_am@163.com>
2014-12-13 10:22:42 +0800

    add openssl.cnf openssl.sh

---
+++ openssl.sh
@@ -0,0 +1,6 @@
+﻿openssl genrsa 2048 > ca/ca.key
+openssl req -new -x509 -config openssl.cnf -days 3653 -nodes -key ca/ca.key -out ca/ca.crt
+
+openssl genrsa 2048 > certs/user.key
+openssl req -new -config openssl.cnf -key certs/user.key -out certs/user.req
+openssl ca -config openssl.cnf -in certs/user.req -cert ca/ca.crt -keyfile ca/ca.key -out certs/user.crt
