b78a716b93292d390270c94ec346d2331ee711c6
xw_y_am@rmbp <xw_y_am@163.com>
2018-04-29 23:23:50 +0800

    zsh: add note rsync function

--- home/user/zsh/function.zsh
+++ home/user/zsh/function.zsh
@@ -2,3 +2,11 @@
 pip_update() {
   pip install -U $(pip freeze | sed -e 's/==.*//g')
 }
+
+note_push() {
+  rsync -avu --exclude=.DS_Store Documents/notes server:/data/documents/
+}
+
+note_pull() {
+  rsync -avu server:/data/documents/notes Documents/
+}
