dd148f159dd4028babe276df7253a025dd1760a3
xw_y_am <xw_y_am@163.com>
2018-06-03 18:53:27 +0800

    update

--- home/user/zsh/exports.zsh
+++ home/user/zsh/exports.zsh
@@ -9,4 +9,4 @@ export GCC_COLORS="error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quo
 export LANG=zh_CN.UTF-8
 export LANGUAGE=en_US:zh_CN
 
-export PATH='/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/Wireshark.app/Contents/MacOS'
+export PATH='/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/Wireshark.app/Contents/MacOS:/Users/xwy/Library/Python/3.6/bin:/Library/Qt5.10.1/5.10.1/clang_64/bin/'
--- home/user/zsh/function.zsh
+++ home/user/zsh/function.zsh
@@ -2,11 +2,3 @@
 pip_update() {
   pip install -U $(pip freeze | sed -e 's/==.*//g')
 }
-
-note_push() {
-  rsync -avu --exclude=.DS_Store Documents/notes server:/data/documents/
-}
-
-note_pull() {
-  rsync -avu server:/data/documents/notes Documents/
-}
