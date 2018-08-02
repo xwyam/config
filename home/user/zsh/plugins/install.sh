0d8541ed2bdaf06f7d43c8f45725dd01b5a8b3de
xw_y_am <xw_y_am@163.com>
2018-08-02 11:16:31 +0000

    update zsh config

---
+++ home/user/zsh/plugins/install.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions zsh-autosuggestions
+
+aria2c https://raw.githubusercontent.com/Angelmmiguel/pm/master/zsh/pm.zsh
+
---
+++ home/user/zsh/plugins/load.zsh
@@ -0,0 +1,3 @@
+source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+#source ~/.zsh/plugins/pm.zsh
