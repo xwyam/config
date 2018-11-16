4a360e0b22ebb870cfb32b4f723972aa14a53033
xw_y_am <xw_y_am@163.com>
2018-11-16 22:37:35 +0800

    update zsh plugins install.sh

--- home/user/zsh/plugins/install.sh
+++ home/user/zsh/plugins/install.sh
@@ -1,8 +1,5 @@
 cd plugged
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git zsh-syntax-highlighting
-
-git clone https://github.com/zsh-users/zsh-autosuggestions zsh-autosuggestions
-
-aria2c https://raw.githubusercontent.com/Angelmmiguel/pm/master/zsh/pm.zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+git clone https://github.com/Angelmmiguel/pm.git
 
