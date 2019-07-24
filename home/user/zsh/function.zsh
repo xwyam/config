a843b1ab04ef1b57f29048a090bdca398c7a3870
xuwenyao(x00483357) <xuwenyao@huawei.com>
2019-07-24 14:27:31 +0800

    add search

--- home/user/zsh/function.zsh
+++ home/user/zsh/function.zsh
@@ -2,3 +2,15 @@
 pip_update() {
   pip install -U $(pip freeze | sed -e 's/==.*//g')
 }
+
+search_build() {
+    grep "$1" . -rn --color --include='Makefile'
+    grep "$1" . -rn --color --include='*.mk'
+    grep "$1" . -rn --color --include='*.sh'
+}
+
+search_source() {
+    grep "$1" . -rn --color --include='*.c'
+    grep "$1" . -rn --color --include='*.h'
+}
+
